331e641becb90b1dff2cc93d292e0bfb1fc0c97a
Pvchan <pvc.pranav@gmail.com>
2023-07-31 19:47:58 +0930

    payment kinda done

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,9 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
 from .views.home import Index , store
 from .views.signup import Signup
 from .views.login import Login , logout
 from .views.cart import Cart
-#from .views.checkout import CreatePaymentIntentView
 from .views.orders import OrderView
 from .middlewares.auth import  auth_middleware
 from .views.products import ProductView
@@ -18,12 +17,11 @@ from .views.viewusers import ViewUsers, EditUser
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
     path('store', store , name='store'),
-
     path('signup', Signup.as_view(), name='signup'),
     path('login', Login.as_view(), name='login'),
     path('logout', logout , name='logout'),
     path('cart', auth_middleware(Cart.as_view()) , name='cart'),
-    #path('check-out', CreatePaymentIntentView.as_view() , name='checkout'),
+    path('checkout', auth_middleware(views.cart.checkout) , name='checkout'),
     path('orders', auth_middleware(OrderView.as_view()), name='orders'),
     path('product/<int:product_id>', ProductView.as_view(), name='product'),
     path('admin_dashboard', AdminView.as_view(), name='admin_dashboard'),
--- store/views/cart.py
+++ store/views/cart.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render , redirect
-
-from django.contrib.auth.hashers import  check_password
-from store.models.user import CustomUser
-from django.views import  View
+from django.shortcuts import render 
+import stripe
+from django.views import View
 from store.models.products import Products
+from django.conf import settings
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 class Cart(View):
     def get(self , request):
@@ -12,3 +13,17 @@ class Cart(View):
         print(products)
         return render(request , 'cart.html' , {'products' : products} )
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['key'] = settings.STRIPE_PUBLIC_KEY
+        return context
+    
+def checkout(request):
+    if request.method == "POST":
+        checkout = stripe.Charge.create(
+            amount=100,
+            currency="aud",
+            description="Payment",
+            source=request.POST['stripeToken']
+        )
+    return render(request , 'checkout.html')

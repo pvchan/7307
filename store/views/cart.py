3522401f3ad155884bd0844629887ec127497611
sarfarazahamedrizwanbasha <124761816+sarfarazahamedrizwanbasha@users.noreply.github.com>
2023-07-31 23:11:03 +0930

    Update cart.py for Payment and Order functionality

--- store/views/cart.py
+++ store/views/cart.py
@@ -3,6 +3,8 @@ import stripe
 from django.views import View
 from store.models.products import Products
 from django.conf import settings
+from store.models.user import CustomUser
+from store.models.orders import Order
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -18,6 +20,7 @@ class Cart(View):
         context['key'] = settings.STRIPE_PUBLIC_KEY
         return context
     
+#@login_required
 def checkout(request):
     if request.method == "POST":
         checkout = stripe.Charge.create(
@@ -26,4 +29,33 @@ def checkout(request):
             description="Payment",
             source=request.POST['stripeToken']
         )
+
+        if checkout['status'] == 'succeeded':
+            customer_id = request.session.get('customer')
+            customer = CustomUser.objects.get(id=customer_id)
+
+            cart = request.session.get('cart')
+            for product_id, quantity in cart.items():
+                product = Products.objects.get(id=product_id)
+
+                # Retrieve address and phone number from the POST data
+                address = request.POST.get('address')
+                phone = request.POST.get('phone')
+
+                # create order
+                order = Order(
+                    product=product,
+                    customer=customer,
+                    user=customer,
+                    quantity=quantity,
+                    price=product.price,
+                    address=address,
+                    phone=phone,
+                )
+                order.placeOrder()
+
+            # Clear cart
+            request.session['cart'] = {}
+
     return render(request , 'checkout.html')
+

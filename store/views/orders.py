f2606e8adf25812fb51e12f488a17695fc7a4a18
Pvchan <pvc.pranav@gmail.com>
2023-08-05 14:19:43 +0930

    cart and orders are restricted

--- store/views/orders.py
+++ store/views/orders.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
-from django.views import View
+from django.shortcuts import render, redirect
 from store.models.orders import Order
+from store.models.user import UserRole, CustomUser
+from django.views import View
 
 class OrderView(View):
+    def get(self, request):
+        customer_id = request.session.get('customer')
+        roles = []
+        is_customer = False
+        if customer_id:
+            try:
+                customer = CustomUser.objects.get(id=customer_id)
+                user_roles = UserRole.objects.filter(user=customer)
+                for user_role in user_roles:
+                    role_name = user_role.role.name.lower()
+                    roles.append(role_name)
+                    if role_name == 'customer':
+                        is_customer = True
+                        break
+            except CustomUser.DoesNotExist:
+                pass
 
-    def get(self , request ):
-        customer = request.session.get('customer')
-        orders = Order.get_orders_by_customer(customer)
-        print(orders)
-        return render(request , 'orders.html'  , {'orders' : orders})
+        if is_customer:
+            orders = Order.objects.filter(customer=customer).order_by('-date')
+            return render(request, 'orders.html', {'orders': orders, 'roles': roles})
+        else:
+            return render(request, 'login.html', {'error': 'You must be a customer to access your orders!'})

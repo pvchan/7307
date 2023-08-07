fb906efb41bc11fe86059c719c2110bf0ca8fc60
Pvchan <pvc.pranav@gmail.com>
2023-08-07 16:20:21 +0930

    cart sorted

--- store/views/cart.py
+++ store/views/cart.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render 
+from django.shortcuts import render
+from django.http import HttpResponseForbidden
 import stripe
 from django.views import View
 from store.models.products import Products
@@ -33,40 +34,50 @@ class Cart(View):
         return context
     
 def checkout(request):
-    if request.method == "POST":
-        checkout = stripe.Charge.create(
-            amount=100,
-            currency="aud",
-            description="Payment",
-            source=request.POST['stripeToken']
-        )
+    customer_id = request.session.get('customer')
+    roles = []
+    if customer_id:
+        customer = CustomUser.objects.get(id=customer_id)
+        user_roles = UserRole.objects.filter(user=customer)
+        for user_role in user_roles:
+            role_name = user_role.role.name.lower()
+            roles.append(role_name)
+            if role_name == 'customer':
+                if request.method == "POST":
+                    checkout = stripe.Charge.create(
+                        amount=100,
+                        currency="aud",
+                        description="Payment",
+                        source=request.POST['stripeToken']
+                    )
 
-        if checkout['status'] == 'succeeded':
-            customer_id = request.session.get('customer')
-            customer = CustomUser.objects.get(id=customer_id)
+                    if checkout['status'] == 'succeeded':
+                        customer_id = request.session.get('customer')
+                        customer = CustomUser.objects.get(id=customer_id)
 
-            cart = request.session.get('cart')
-            for product_id, quantity in cart.items():
-                product = Products.objects.get(id=product_id)
+                        cart = request.session.get('cart')
+                        for product_id, quantity in cart.items():
+                            product = Products.objects.get(id=product_id)
 
-                # Retrieve address and phone number from the POST data
-                address = request.POST.get('address')
-                phone = request.POST.get('phone')
+                            # Retrieve address and phone number from the POST data
+                            address = request.POST.get('address')
+                            phone = request.POST.get('phone')
 
-                # create order
-                order = Order(
-                    product=product,
-                    customer=customer,
-                    user=customer,
-                    quantity=quantity,
-                    price=product.price,
-                    address=address,
-                    phone=phone,
-                )
-                order.placeOrder()
+                            # create order
+                            order = Order(
+                                product=product,
+                                customer=customer,
+                                user=customer,
+                                quantity=quantity,
+                                price=product.price,
+                                address=address,
+                                phone=phone,
+                            )
+                            order.placeOrder()
 
-            # Clear cart
-            request.session['cart'] = {}
+                        # Clear cart
+                        request.session['cart'] = {}
 
-    return render(request , 'checkout.html')
-
+                    return render(request , 'checkout.html')
+                else:
+                    return HttpResponseForbidden()

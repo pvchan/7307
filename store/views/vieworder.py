6c9c885d919d4fcb0694b709a0c54c0abe67d520
SampadaNeupane71 <70383954+SampadaNeupane71@users.noreply.github.com>
2023-08-13 13:49:48 +0930

    CSRF Prevention added

--- store/views/vieworder.py
+++ store/views/vieworder.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from store.models.user import CustomUser, UserRole
 from store.models.orders import Order
+from django.middleware.csrf import rotate_token
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
 
+@method_decorator(csrf_protect, name='dispatch')
 class ViewOrders(View):
     def get(self, request):
         # Get user roles
@@ -25,12 +29,14 @@ class ViewOrders(View):
         # Redirect to login or other page if the user does not have the required role
         return render(request, 'login.html', {'error': 'You must be admin or staff !!'})
 
+@method_decorator(csrf_protect, name='dispatch')
 class EditOrder(View):
     def get(self, request, order_id):
         order = get_object_or_404(Order, id=order_id)
         return render(request, 'editorder.html', {'order': order})
 
     def post(self, request, order_id):
+        rotate_token(request)
         order = get_object_or_404(Order, id=order_id)
         status = request.POST.get('status')
         order.status = True if status == 'paid' else False
